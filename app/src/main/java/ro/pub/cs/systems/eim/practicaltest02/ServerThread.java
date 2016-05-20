c5ef4dbb77cc874a07fc04d8eff7b26afb14b83d
ioanateodorescu94 <ioana.teodorescu94@gmail.com>
2016-05-20 13:29:42 +0300

    mesaj

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/ServerThread.java
@@ -0,0 +1,87 @@
+package ro.pub.cs.systems.eim.practicaltest02;
+
+import android.util.Log;
+
+import org.apache.http.client.ClientProtocolException;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+import ro.pub.cs.systems.eim.practicaltest02.Constants;
+
+
+public class ServerThread extends Thread {
+
+    private int port = 0;
+    private ServerSocket serverSocket = null;
+
+
+
+    public ServerThread(int port) {
+        this.port = port;
+        try {
+            this.serverSocket = new ServerSocket(port);
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        }
+
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setServerSocker(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                Log.i(Constants.TAG, "[SERVER] Waiting for a connection...");
+                Socket socket = serverSocket.accept();
+                Log.i(Constants.TAG, "[SERVER] A connection request was received from " + socket.getInetAddress() + ":" + socket.getLocalPort());
+            }
+        } catch (ClientProtocolException clientProtocolException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + clientProtocolException.getMessage());
+            if (Constants.DEBUG) {
+                clientProtocolException.printStackTrace();
+            }
+        } catch (IOException ioException) {
+            Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+            if (Constants.DEBUG) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public void stopThread() {
+        if (serverSocket != null) {
+            interrupt();
+            try {
+                serverSocket.close();
+            } catch (IOException ioException) {
+                Log.e(Constants.TAG, "An exception has occurred: " + ioException.getMessage());
+                if (Constants.DEBUG) {
+                    ioException.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
